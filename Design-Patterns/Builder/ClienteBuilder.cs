f9f7a07cdffa839abba9f1deb8ced94e462f641e
Richard Vaz De Teixeira <richard.teixeira@farroupilha.local>
2026-09-23 21:36:34 -0300

    Criado pasta sobre builder

---
+++ Design-Patterns/Builder/ClienteBuilder.cs
@@ -0,0 +1,32 @@
+﻿public class ClienteBuilder
+{
+    private Cliente NovoCliente { get; } = new Cliente();
+
+    public ClienteBuilder CriarNovo(string nome, int idade)
+    {
+        NovoCliente.Nome = nome;
+        NovoCliente.Idade = idade;
+        return this;
+    }
+
+    public ClienteBuilder AdicionarContato(string email, string dddTelefone, string numeroTelefone)
+    {
+        NovoCliente.Email = email;
+        NovoCliente.DddTelefone = dddTelefone;
+        NovoCliente.NumeroTelefone = numeroTelefone;
+        return this;
+    }
+
+    public ClienteBuilder AdicionarEndereco(string cidade, string cep, string estado, int numero, string bairro, string rua)
+    {
+        NovoCliente.Cidade = cidade;
+        NovoCliente.Cep = cep;
+        NovoCliente.Estado = estado;
+        NovoCliente.Numero = numero;
+        NovoCliente.Bairro = bairro;
+        NovoCliente.Rua = rua;
+        return this;
+    }
+
+    public Cliente Build() => NovoCliente;
+}
